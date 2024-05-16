622e6d54b29703011a16232bcf81412f3c6e9d85
Tatiana <tstwrt7@gmail.com>
2024-05-16 14:14:38 -0400

    Created instance of Flask and db

--- app.py
+++ app.py
@@ -1,18 +1,45 @@
 # Import Dependencies
-from flask import Flask, jsonify
+from flask import Flask
+from flask_cors import CORS
+
+# Import Configurations
+from server.connection.database import db
+
+# Import Seed Data Function
 
 
 
 
 
 # Create an instance of Flask
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    # Import Routes
+
+    # Register Blueprints
 
 
+    with app.app_context():
+        db.create_all()
+        # Seed Data Function
+
+    return app
 
 
-# Define Routes
+# Database Setup
+app = create_app()
 
+
+# Run the application
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
