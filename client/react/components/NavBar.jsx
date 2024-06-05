72f98fa21187298ef05e9542b7ab5560555859af
Tatiana <tstwrt7@gmail.com>
2024-06-04 21:17:55 -0400

    Added conditional for navbar buttons view

--- client/react/components/NavBar.jsx
+++ client/react/components/NavBar.jsx
@@ -1,14 +1,28 @@
 import React, { useState, useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 // Import Components
 import LogoutButton from "./Logout";
+import LoginButton from "./Login";
 
-function NavBar() {
+function NavBar({ generatePasswordView }) {
+    const { user, isAuthenticated } = useAuth0();
+
 
     return (
         <div className="flex-div">
             <h1>Password Manager</h1>
-            <LogoutButton /><hr/>
+            
+            <div id="right-nav">
+            {isAuthenticated ?
+                <>
+                    <img id="avatar" src={user.picture} alt={user.name} />
+                    <LogoutButton /><hr/>
+                </>
+            : !isAuthenticated && generatePasswordView && 
+                <LoginButton />
+            }
+            </div>
         </div>
     )
 }
