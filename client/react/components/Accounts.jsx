f410d5ba1595596d42b908c90b48389f98ec97a2
Tatiana <tstwrt7@gmail.com>
2024-06-06 13:23:33 -0400

    Conditional styling/rendering for account data

--- client/react/components/Accounts.jsx
+++ client/react/components/Accounts.jsx
@@ -4,6 +4,8 @@ import { useAuth0 } from "@auth0/auth0-react";
 // Import Assets
 import edit from "../../assets/edit-btn.png";
 import trash from "../../assets/trash-btn.png";
+import show from "../../assets/show.jpeg"
+import hide from "../../assets/hide.jpeg"
 
 
 function Accounts({ userAccounts }) {
@@ -11,6 +13,7 @@ function Accounts({ userAccounts }) {
 
     // State to Show/Hide Credentials
     const [showCredentials, setShowCredentials] = useState({});
+    const [showPassword, setShowPassword] = useState(false);
 
     function toggleCredentials(accountId) {
         setShowCredentials(prevState => ({
@@ -36,15 +39,33 @@ function Accounts({ userAccounts }) {
                     </div>
                     {/* If the Credential View State is True, display Account Credentials for selected Account */}                    
                     {showCredentials[account.id] && (
-                        <div className="card-body">
-                            <div id="left">
-                                <p className="card-text">Username: {account.username}</p>
-                                <p className="card-text">Email: {account.email}</p>
+                        <>
+                            <div className="card-body">
+                                {/* If the Account has a Username, display Username and Email on the left */}
+                                {account.username ? (
+                                    <div id="left">
+                                        <p className="card-text">Username: {account.username}</p>
+                                        <p className="card-text">Email: {account.email}</p>
+                                    </div>    
+                                ) : (
+                                    // If the Account does not have a Username, display Email on the left and Password on the right
+                                    <>
+                                        <p className="card-text">Email: {account.email}</p>
+                                    </>
+                                )}
+                                <div id="right">
+                                    {/* If Show Password is true, reveal the password */}
+                                    {showPassword ? (
+                                        <p className="card-text">Password: {account.password}</p>
+                                    ) : (
+                                        <p className="card-text">Password: ********</p>
+                                    )}
+                                    <div id="password-button-div">
+                                        <button id="show-password" onClick={() => setShowPassword(!showPassword)}><img src={!showPassword ? show : hide}></img></button>
+                                    </div>
+                                </div>
                             </div>
-                            <div id="right">
-                                <p className="card-text">Password: {account.password}</p>
-                            </div>
-                        </div>
+                        </>
                     )}
                 </div>
                 ))
